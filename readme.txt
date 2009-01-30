5e4ec0f5a1f8434940d01c3cf98ff7ad88e7eb8e
filosofo <filosofo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-30 14:37:23 +0000

    fix for people using "the_content" in odd places

---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Plugin Name ===
+Contributors: filosofo
+Donate link: http://pressedwords.com/solving-wordpress-seo-paged-comments-problem/
+Tags: comments, seo, paged-comments, paged
+Requires at least: 2.7
+Tested up to: 2.8
+Stable tag: 1.1
+
+Reduce SEO problems when using WordPress's paged comments.
+
+== Description ==
+
+Reduce SEO problems when using WordPress's paged comments.  See [this page for details](http://pressedwords.com/solving-wordpress-seo-paged-comments-problem/)
+
+== Installation ==
+
+1. Upload `seo-for-paged-comments.php` to the `/wp-content/plugins/` directory, or its own sub-directory
+1. Activate it through the 'Plugins' menu in WordPress
---
+++ seo-for-paged-comments.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ Plugin Name: SEO for Paged Comments
+ Version: 1.1
+ Description: Reduce SEO problems when using WordPress's paged comments.
+ Author: Austin Matzko
+ Author URI: http://www.pressedwords.com
+ */
+
+function seo_paged_comments_content_filter($t = '') {
+	if ( function_exists('get_query_var') ) {
+		$cpage = intval(get_query_var('cpage'));
+		if ( ! empty( $cpage ) ) {
+			remove_filter('the_content', 'seo_paged_comments_content_filter');
+			$t = get_the_excerpt();
+			$t .= sprintf('<p><a href="%1$s">%2$s</a></p>', get_permalink(), get_the_title());
+		}
+	}
+	return $t;
+}
+
+add_filter('the_content', 'seo_paged_comments_content_filter');
